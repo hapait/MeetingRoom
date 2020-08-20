08a3c7fe36021298b48c27e1b22280c83ad56e66
hapait <s.jamil1993@gmail.com>
2020-08-20 16:51:22 +0600

    Meeting tile ok, upto 16 videos

--- src/MeetingTile.js
+++ src/MeetingTile.js
@@ -1,18 +1,19 @@
-import React, { Component, Box, Button} from 'react';
+import React, { Component, Box, Button } from 'react';
 export default class MeetingTile extends React.Component {
     constructor(props) {
         super(props);
-        const pad = 5;
-        this.state = { 
-            n: 16,
-            width: 0, 
-            height: 0,
-            points: [],
+        this.state = {
+            n: 5,
+            //wid: window.innerWidth,
+            //hei: window.innerHeight,
+            wid: 0,
+            hei: 0,
             tileW: 0,
             tileH: 0,
+            row: 0,
+            col: 0,
             test: 0,
             segments: [],
-            testarr: []
         };
         this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
         this.calculateTileSize = this.calculateTileSize.bind(this);
@@ -28,46 +29,64 @@ export default class MeetingTile extends React.Component {
     }
 
     updateWindowDimensions() {
-        this.setState({ width: window.innerWidth, height: window.innerHeight });
+        this.setState({ wid: window.innerWidth, hei: window.innerHeight });
     }
-    calculateTileSize(){
+    calculateTileSize() {
         var sx = Math.ceil(Math.sqrt(this.state.n));
         var ro = Math.ceil(this.state.n / sx);
         var co = Math.ceil(this.state.n / ro);
-        this.setState({test: ro});
+        //this.setState({test: ro});
 
-        if(this.state.n == 7){
-            this.setState({segments: [4, 3], testarr: [4, 3]});
-        }else if(this.state.n == 8){
-            this.setState({segments: [4, 4], testarr: [4,4]});
-        }else{
+        if (this.state.n == 7) {
+            this.setState({ segments: [4, 3], testarr: [4, 3] });
+        } else if (this.state.n == 8) {
+            this.setState({ segments: [4, 4], testarr: [4, 4] });
+        } else {
             var temp = []
             var n2 = this.state.n;
-            while(1){
+            while (1) {
                 n2 = n2 - co;
-                if(n2>co)
-                temp.push(co);
+                if (n2 > co)
+                    temp.push(co);
                 else {
                     temp.push(co);
                     temp.push(n2);
                     break;
                 }
             }
-            this.setState({segments: temp});
+            var x = Math.ceil(this.state.width / ro);
+            //var x = Math.ceil(window.innerWidth / ro);
+            var y = Math.ceil(this.state.height / co);
+            //var y = Math.ceil(window.innerHeight / co);
+            //this.setState({ segments: temp, tileW: y, tileH: x });
+            this.setState({segments: temp, row: co, col: ro});
         }
-    }
 
+    }
+    //<video key={i} autoPlay={true} src="videos/sampleVid.mp4" controls={false}></video>
     render() {
         return (
-            <div style={{flex: 1, alignContent: "center", alignItems: "center"}}>
-                {this.state.width}{this.state.height}{"test: "+this.state.test}
-                {this.state.segments.map((i)=>
-                <p key={i}>row {i} </p>
-                )}
-                {[...Array(this.state.n)].map(
-                    (e, i) => 
-                    <video key={i} autoPlay={true} src="videos/sampleVid.mp4" controls={false}></video>
-                    )}
+            <div style={{flex: 1}}>
+                {
+                    this.state.segments.map((k) =>
+                        <div style={{display: "flex", justifyContent:'center', alignItems:'center', flexDirection: "row"}}>
+                            {[...Array(k)].map((i) =>
+                                <video
+                                    key={i}
+                                    autoPlay={true}
+                                    src="videos/sampleVid.mp4"
+                                    controls={false}
+                                    style={{ 
+                                        width: (window.innerWidth / this.state.row) - 10, 
+                                        height: window.innerHeight / this.state.col - 10,
+                                        alignSelf: "center"
+                                    }}
+                                >
+                                </video>
+                            )}
+                        </div>
+                    )
+                }
             </div>
         )
     }
